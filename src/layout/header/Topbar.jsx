e34a636c3102b3321fd05adabc70f2a8a0f863a1
Riyaz <riyazofficeid@gmail.com>
2025-01-18 08:53:52 +0530

    test1

--- src/layout/header/Topbar.jsx
+++ src/layout/header/Topbar.jsx
@@ -1,7 +1,7 @@
 const Topbar = () => {
   return (
     <h3 className="flex flex-row items-center justify-center gap-2 bg-cyan-800 text-white text-md md:text-lg font-medium py-2">
-      50% Off on Coupon Code: <div className="tracking-wide">NEWYEAR</div>
+      70% Off on Coupon Code: <div className="tracking-wide">NEWYEAR</div>
     </h3>
   );
 };
