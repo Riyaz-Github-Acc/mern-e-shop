245455ba482fcc9a791f5e3dbcbf18924667640e
Riyaz <riyazofficeid@gmail.com>
2025-03-03 01:38:39 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     ssr: true,
+    ssrInternal: true,
+    // Specify the SSR entry point
+    ssrEntry: 'src/entry-server.jsx',
   },
 })
